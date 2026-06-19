3a7fbd5813f633dba45638f7f93d2096c192a8df
lalitutwal <lalitutwal@gmail.com>
2026-06-19 11:37:56 +0530

    Created verify_module.

---
+++ face-verify-api/verify_module.py
@@ -0,0 +1,54 @@
+import os
+import face_recognition
+
+
+def image_exists(
+	image_path):
+    if not os.path.exists(image_path):
+        return False
+    return True
+
+
+def create_encoding(
+	image_path):
+    print(f"[INFO] Creating encoding for {image_path}...")
+    img = face_recognition.load_image_file(image_path)
+    try:
+        encoding = face_recognition.face_encodings(img)[0]
+        return encoding
+    except IndexError:
+        return {"error": f"No face found in {image_path}."}
+
+
+def verify_encodings(
+	encoding_1,
+	encoding_2):
+    print("[INFO] verify_encodings function initiated...")
+    result = face_recognition.compare_faces([encoding_1], encoding_2, tolerance=0.6)
+    return result
+
+
+def verify_images(
+	image_path_1,
+	image_path_2):
+    print("[INFO] verify_images function initiated...")
+
+    # Explicitly check if files exist before trying to load them
+    if not image_exists(image_path_1):
+        return {"error": f"File missing: {image_path_1}"}
+    if not image_exists(image_path_2):
+        return {"error": f"File missing: {image_path_2}"}
+ 
+
+    print("[INFO] Loading and Generating face encodings...")
+    encoding_1 = create_encoding(image_path_1)
+    encoding_2 = create_encoding(image_path_2)
+
+    matches = verify_encodings(encoding_1, encoding_2)
+
+   # distance = face_recognition.face_distance([encoding_1], encoding_2)[0]
+
+    return {
+        "same_person": bool(matches[0]),
+        #"distance_score": round(float(distance), 4),
+    }
